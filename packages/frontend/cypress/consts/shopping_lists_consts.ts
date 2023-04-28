8a87903bd0533c977bb328142048356c1d33b68b
Elizabeth Cho <zergling13rush@gmail.com>
2023-04-28 03:39:51 -0400

    shopping lists ui tests

---
+++ packages/frontend/cypress/consts/shopping_lists_consts.ts
@@ -0,0 +1,88 @@
+// create a new list
+export const NEW_SHOPPING_LIST = '#main-content > page-shopping-lists > ion-content > ion-fab > ion-fab-button';
+export const NEW_LIST_TITLE_INPUT = 'ion-modal > page-new-shopping-list-modal > ion-content > ion-item > ion-input > input';
+export const NEW_LIST_CREATE = 'ion-modal > page-new-shopping-list-modal > ion-footer > ion-button';
+export const NEW_LIST_BACK = '#main-content > page-shopping-list > ion-header > ion-toolbar > ion-buttons.buttons-first-slot.sc-ion-buttons-md-h.sc-ion-buttons-md-s.md.hydrated > ion-back-button';
+export const NEW_LIST_TITLE_SECOND = '#main-content > page-shopping-lists > ion-content > ion-list > ion-item:nth-child(2) > ion-label > h2';
+export const SECOND_IN_LIST = '#main-content > page-shopping-lists > ion-content > ion-list > ion-item:nth-child(2)';
+export const FIRST_IN_LIST = '#main-content > page-shopping-lists > ion-content > ion-list > ion-item:nth-child(1)';
+
+// options
+export const OPTIONS_NO_MULTISELECT = '#main-content > page-shopping-list > ion-header > ion-toolbar > ion-buttons.buttons-last-slot.sc-ion-buttons-md-h.sc-ion-buttons-md-s.md.hydrated > ion-button';
+export const DELETE_SHOPPING_LIST = 'ion-popover > page-shopping-list-popover > ion-list > ion-button:nth-child(10)';
+export const REMOVE_ALL_ITEMS = 'ion-popover > page-shopping-list-popover > ion-list > ion-button:nth-child(9)';
+export const PRINT_RECIPE = 'ion-popover > page-shopping-list-popover > ion-list > ion-button:nth-child(8)';
+export const DELETE_UPON_COMPLETION_TOGGLE = 'ion-popover > page-shopping-list-popover > ion-list > ion-item:nth-child(7) > ion-toggle';
+export const SHOW_ADDED_ON_TOGGLE = 'ion-popover > page-shopping-list-popover > ion-list > ion-item:nth-child(6) > ion-toggle';
+export const SHOW_RECIPE_TITLE_TOGGLE = 'ion-popover > page-shopping-list-popover > ion-list > ion-item:nth-child(5) > ion-toggle';
+export const GROUP_SIMILAR_ITEMS_TOGGLE = 'ion-popover > page-shopping-list-popover > ion-list > ion-item:nth-child(4) > ion-toggle';
+export const GROUP_ITEMS_BY_CATEGORY_TOGGLE = 'ion-popover > page-shopping-list-popover > ion-list > ion-item:nth-child(3) > ion-toggle';
+export const SORT_BY = 'ion-popover > page-shopping-list-popover > ion-list > ion-item:nth-child(2) > ion-select';
+export const DELETE_CONFIRM = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-button-group.sc-ion-alert-md > button.alert-button.ion-focusable.ion-activatable.alertDanger.sc-ion-alert-md';
+export const DELETE_CANCEL = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-button-group.sc-ion-alert-md > button.alert-button.ion-focusable.ion-activatable.alert-button-role-cancel.sc-ion-alert-md'
+export const DELETE_ALL_CONFIRM = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-button-group.sc-ion-alert-md > button.alert-button.ion-focusable.ion-activatable.alertDanger.sc-ion-alert-md';
+export const DELETE_ALL_CANCEL = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-button-group.sc-ion-alert-md > button.alert-button.ion-focusable.ion-activatable.alert-button-role-cancel.sc-ion-alert-md';
+
+// options - sort by
+export const SORT_BY_POPUP = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md';
+export const SORT_BY_TITLE = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-radio-group.sc-ion-alert-md > button:nth-child(1)';
+export const CREATED_NEWEST = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-radio-group.sc-ion-alert-md > button:nth-child(2)';
+export const CREATED_OLDEST = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-radio-group.sc-ion-alert-md > button:nth-child(3)';
+export const SORT_BY_CANCEL = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-button-group.sc-ion-alert-md > button:nth-child(1)';
+export const SORT_BY_OK = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-button-group.sc-ion-alert-md > button:nth-child(2)'
+
+// options - select
+export const HALF_CUP_MILK = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(1) > div > div:nth-child(1) > shopping-list-group > shopping-list-item > ion-item';
+export const COMPLETED_ITEMS_LIST = '#main-content > page-shopping-list > ion-content > ion-list:nth-child(6)';
+export const RESTORE_COMPLETED = '#main-content > page-shopping-list > ion-content > ion-list:nth-child(6) > ion-item > ion-button.ion-color.ion-color-primary.md.button.button-small.button-solid.button-has-icon-only.ion-activatable.ion-focusable.hydrated';
+
+// options - group items by category
+export const DAIRY_HEADER = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(1) > ion-list-header';
+export const GROCERY_HEADER = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(2) > ion-list-header';
+export const PRODUCE_HEADER = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(3) > ion-list-header';
+
+// options - group similar items
+export const SUGAR_GROUPED = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(2) > div > div:nth-child(1)';
+export const GROUP_OPTION_CUP_SUGAR = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(2) > div > div:nth-child(1)';
+export const GROUP_OPTION_TBSP_SUGAR = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(2) > div > div:nth-child(2)';
+
+// options - added on
+export const ADDED_ON_LABEL = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(1) > div > div:nth-child(1) > shopping-list-group > shopping-list-item';
+
+// add items
+export const ADD_ITEMS_BOTTOM_CORNER = '#main-content > page-shopping-list > ion-content > ion-fab > ion-fab-button';
+export const ADD_ITEMS_CENTER = '#main-content > page-shopping-list > ion-content > ion-list > null-state > ion-grid > ion-row.ion-text-center.ion-text-wrap.ion-margin-top.ion-justify-content-around.md.hydrated > ion-label > p > ion-button';
+export const ADD_ITEMS_MODAL = 'ion-modal > page-new-shopping-list-item-modal';
+export const ADD_ITEMS_CLOSE = 'ion-modal > page-new-shopping-list-item-modal > ion-header > ion-toolbar > ion-buttons > ion-button';
+export const ADD_ITEMS_ADD_TO_LIST = 'ion-modal > page-new-shopping-list-item-modal > ion-footer > ion-button';
+
+// added items
+export const ADDED_STRAWBERRIES = '#main-content > page-shopping-list > ion-content > ion-list > div > div > div > div > shopping-list-group > shopping-list-item > ion-item';
+export const ADDED_STRAWBERRIES_MULTIPLE = '#main-content > page-shopping-list > ion-content > ion-list > div > div > div > div:nth-child(2)';
+export const ADDED_BLUEBERRIES_MULTIPLE = '#main-content > page-shopping-list > ion-content > ion-list > div > div > div > div:nth-child(1)';
+export const REMOVE_SELECTED_ITEM = '#main-content > page-shopping-list > ion-content > ion-list:nth-child(5) > ion-item > ion-button.ion-color.ion-color-danger.md.button.button-small.button-solid.button-has-icon-only.ion-activatable.ion-focusable.hydrated';
+export const REMOVE_ITEM_CONFIRM = 'ion-alert > div.alert-wrapper.ion-overlay-wrapper.sc-ion-alert-md > div.alert-button-group.sc-ion-alert-md > button.alert-button.ion-focusable.ion-activatable.alertDanger.sc-ion-alert-md';
+export const COMBINED_SUGAR = '#main-content > page-shopping-list > ion-content > ion-list > div > div > div';
+export const COMBINED_SUGAR_DROPDOWN = '#main-content > page-shopping-list > ion-content > ion-list > div > div > div > div > shopping-list-group > div > ion-grid > ion-row > ion-col:nth-child(2) > ion-button';
+export const CUP_SUGAR = '#main-content > page-shopping-list > ion-content > ion-list > div > div > div > div > shopping-list-group > div > ion-grid > ion-row.indented-item.md.hydrated > ion-col > shopping-list-item:nth-child(1)';
+export const TBSP_SUGAR = '#main-content > page-shopping-list > ion-content > ion-list > div > div > div > div > shopping-list-group > div > ion-grid > ion-row.indented-item.md.hydrated > ion-col > shopping-list-item:nth-child(2)';
+
+// checking category titles
+export const ADDED_DAIRY = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(1)';
+export const ADDED_FROZEN = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(2)';
+export const ADDED_GROCERY = '#main-content > page-shopping-list > ion-content > ion-list.md.list-md.list-lines-none.list-md-lines-none.hydrated > div > div:nth-child(3)';
+export const SMOOTHIE_RECIPE_TAG = '#main-content > page-shopping-list > ion-content > ion-list:nth-child(4) > ion-item';
+export const SHAMPOO_MEAT = '#main-content > page-shopping-list > ion-content > ion-list > div > div';
+export const TYPO_GROCERY = '#main-content > page-shopping-list > ion-content > ion-list > div > div:nth-child(1)';
+export const TYPO_UNCATEGORIZED = '#main-content > page-shopping-list > ion-content > ion-list > div > div:nth-child(2)';
+
+// add items - searching
+export const ITEM_TITLE_INPUT = 'ion-modal > page-new-shopping-list-item-modal > ion-content > div:nth-child(2) > div > ion-item > ion-input > input';
+export const SECOND_ITEM_TITLE_INPUT = 'ion-modal > page-new-shopping-list-item-modal > ion-content > div:nth-child(2) > div > ion-item.item-interactive.item-input.item.md.item-lines-default.item-fill-none.hydrated.ion-untouched.ion-pristine.ion-valid.item-label.item-label-floating > ion-input > input';
+export const RECIPE_INPUT = 'ion-modal > page-new-shopping-list-item-modal > ion-content > div:nth-child(2) > div > select-recipe > div > div > ion-searchbar > div > input';
+export const RECIPE_FIRST_RESULT = 'ion-modal > page-new-shopping-list-item-modal > ion-content > div:nth-child(2) > div > select-recipe > div > div > div > button:nth-child(1) > ion-item';
+export const FRUIT_SMOOTHIE_RESULT = 'ion-modal > page-new-shopping-list-item-modal > ion-content > div:nth-child(2) > div > select-recipe > div > div > div > button:nth-child(1) > ion-item > ion-label';
+
+// add items - overlay tabs
+export const ADD_ITEMS_RECIPE_TAB = 'ion-modal > page-new-shopping-list-item-modal > ion-content > div:nth-child(1) > ion-segment > ion-segment-button:nth-child(2)';
+export const ADD_ITEMS_ITEMS_TAB = 'ion-modal > page-new-shopping-list-item-modal > ion-content > div:nth-child(1) > ion-segment > ion-segment-button:nth-child(1)';
--- packages/frontend/cypress/e2e/shopping_lists.cy.ts
+++ packages/frontend/cypress/e2e/shopping_lists.cy.ts
@@ -1,42 +1,313 @@
-describe('shopping lists', () => {
+import * as sidebar from '../consts/sidebar_const';
+import * as landing_login from '../consts/landing_login_const';
+import * as shopping_list from '../consts/shopping_lists_consts';
+
+describe('shopping lists - empty form verification', () => {
+    beforeEach(() => {
+        cy.visit('/');
+        cy.get(landing_login.LOGIN_TOP).click();
+        cy.get(':nth-child(8) > .ng-untouched').type('echo30@jhu.edu');
+        cy.get(':nth-child(11) > .ng-untouched').type('123456');
+        cy.get('.ion-padding > .md').click();
+        cy.get(sidebar.OPEN_SIDEBAR).click();
+        cy.get(sidebar.SHOPPING_LISTS).click();
+    })
+
+    // should not be able to create a new shopping list without a name
+    it('should not be able to create a new shopping list without a name', () => {
+        cy.get(shopping_list.NEW_SHOPPING_LIST).click();
+        cy.get(shopping_list.NEW_LIST_CREATE).should('not.be.enabled');
+    })
 
     // need to have a name for the shopping list
+    it('should be able to create a new shopping list with just a name', () => {
+        cy.get(shopping_list.NEW_SHOPPING_LIST).click();
+        cy.get(shopping_list.NEW_LIST_TITLE_INPUT).type('test shopping list');
+        cy.get(shopping_list.NEW_LIST_CREATE).should('not.be.disabled');
+        cy.get(shopping_list.NEW_LIST_CREATE).click();
+        cy.get(shopping_list.NEW_LIST_BACK).click();
+        cy.get(shopping_list.NEW_LIST_TITLE_SECOND).contains('test shopping list');
+        // delete the shopping list to reset
+        cy.get(shopping_list.SECOND_IN_LIST).click();
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.DELETE_SHOPPING_LIST).click();
+        cy.get(shopping_list.DELETE_CONFIRM).click();
+    })
 
-    // can create a new shopping list
-
-    // add items directly to a shopping list
-
-    // add a recipe to the shopping list from the shopping list page (from the recipe page covered already)
-
-    // remove an item from the shopping list
-
-    // changing sort ortder
-
-    // printing a shopping list
-
-    // did they handle shampoo = meat
-
-    // group similar items (1 cup sugar, 1 tbsp sugar)
-
-    // group similar items - typo edition
-
-    // mark item as completed
-
-    // restore completed item
-
-    // delete item immediately
-
-    // sort by
-
-    // group items by category toggle
-
-    // group similar items toggle
-
-    // show added by toggle
-
-    // show added on toggle
-
-    // remove all items
+    it('should not be able to click on add to list when there are no items added', () => {
+        cy.get(shopping_list.NEW_SHOPPING_LIST).click();
+        cy.get(shopping_list.NEW_LIST_TITLE_INPUT).type('test shopping list');
+        cy.get(shopping_list.NEW_LIST_CREATE).click();
+        // no items added
+        cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).should('not.be.enabled');
+        cy.get(shopping_list.ADD_ITEMS_CLOSE).click();
+        // reset
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.DELETE_SHOPPING_LIST).click();
+        cy.get(shopping_list.DELETE_CONFIRM).click();
+    })
 
     // delete list
+    it('should be able to delete a list', () => {
+        cy.get(shopping_list.NEW_SHOPPING_LIST).click();
+        cy.get(shopping_list.NEW_LIST_TITLE_INPUT).type('0. delete shopping list');
+        cy.get(shopping_list.NEW_LIST_CREATE).click();
+        cy.get(shopping_list.NEW_LIST_BACK).click();
+        cy.get(shopping_list.FIRST_IN_LIST).should('exist');
+        cy.get(shopping_list.FIRST_IN_LIST).contains('0. delete shopping list');  
+        cy.get(shopping_list.FIRST_IN_LIST).click();
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.DELETE_SHOPPING_LIST).click();
+        cy.get(shopping_list.DELETE_CONFIRM).click();
+        cy.get(shopping_list.FIRST_IN_LIST).should('not.contain', '0. delete shopping list');  
+    })
+})
+
+describe('shopping lists - creating and editing', () => {
+    beforeEach(() => {
+        cy.visit('/');
+        cy.get(landing_login.LOGIN_TOP).click();
+        cy.get(':nth-child(8) > .ng-untouched').type('echo30@jhu.edu');
+        cy.get(':nth-child(11) > .ng-untouched').type('123456');
+        cy.get('.ion-padding > .md').click();
+        cy.get(sidebar.OPEN_SIDEBAR).click();
+        cy.get(sidebar.SHOPPING_LISTS).click();
+        // new list
+        cy.get(shopping_list.NEW_SHOPPING_LIST).click();
+        cy.get(shopping_list.NEW_LIST_TITLE_INPUT).type('test shopping list');
+        cy.get(shopping_list.NEW_LIST_CREATE).click();
+    })
+
+    afterEach(() => {
+        // delete the shopping list to reset
+        // cy.get(shopping_list.SECOND_IN_LIST).click();
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click({force: true});
+        cy.wait(500);
+        cy.get(shopping_list.DELETE_SHOPPING_LIST).click({force: true});
+        cy.wait(500);
+        cy.get(shopping_list.DELETE_CONFIRM).click({force: true});
+    })
+
+    // add items directly to a shopping list with the bottom corner button - single
+    it('should be able to add items directly to a shopping list using the bottom corner button', () => {
+        // add a single item
+        cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+        cy.get(shopping_list.ADD_ITEMS_MODAL).should('be.visible');
+        cy.get(shopping_list.ITEM_TITLE_INPUT).type('strawberries');
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        cy.get(shopping_list.ADDED_STRAWBERRIES).should('exist');
+        cy.get(shopping_list.ADDED_STRAWBERRIES).contains('strawberries');
+    })
+
+    // add items directly to a shopping list with the center button - multiple
+    it('should be able to add items directly to a shopping list using the center button', () => {
+        cy.get(shopping_list.ADD_ITEMS_CENTER).click();
+        cy.get(shopping_list.ADD_ITEMS_MODAL).should('be.visible');
+        cy.get(shopping_list.ITEM_TITLE_INPUT).type('strawberries');
+        cy.get(shopping_list.SECOND_ITEM_TITLE_INPUT).type('blueberries');
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        cy.get(shopping_list.ADDED_STRAWBERRIES_MULTIPLE).should('exist');  
+        cy.get(shopping_list.ADDED_STRAWBERRIES_MULTIPLE).contains('strawberries');
+        cy.get(shopping_list.ADDED_BLUEBERRIES_MULTIPLE).should('exist');
+        cy.get(shopping_list.ADDED_BLUEBERRIES_MULTIPLE).contains('blueberries');
+    })
+    
+    // add a recipe to the shopping list from the shopping list page (from the recipe page covered already)
+    it('should be able to add recipe ingredients to the shopping list', () => {
+        cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+        cy.get(shopping_list.ADD_ITEMS_RECIPE_TAB).click();
+        cy.get(shopping_list.RECIPE_INPUT);
+        cy.get(shopping_list.RECIPE_INPUT).type('fruit smoothie');
+        cy.get(shopping_list.FRUIT_SMOOTHIE_RESULT).contains('Fruit Smoothie');
+        cy.get(shopping_list.RECIPE_FIRST_RESULT).click();
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        // check
+        cy.get(shopping_list.ADDED_DAIRY).should('exist');
+        cy.get(shopping_list.ADDED_DAIRY).contains('Dairy');
+        cy.get(shopping_list.ADDED_FROZEN).should('exist');
+        cy.get(shopping_list.ADDED_FROZEN).contains('Frozen');
+        cy.get(shopping_list.ADDED_GROCERY).should('exist');
+        cy.get(shopping_list.ADDED_GROCERY).contains('Grocery Items');
+        cy.get(shopping_list.SMOOTHIE_RECIPE_TAG).should('exist');
+        cy.get(shopping_list.SMOOTHIE_RECIPE_TAG).contains('Frozen Fruit Smoothies');
+    })  
+    
+    // remove an item from the shopping list
+    it('should be able to remove an item from the shopping list', () => {
+        cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+        cy.get(shopping_list.ITEM_TITLE_INPUT).type('strawberries');
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        cy.wait(1000);
+        cy.get(shopping_list.ADDED_STRAWBERRIES).click();
+        cy.get(shopping_list.REMOVE_SELECTED_ITEM).click();
+        cy.get(shopping_list.REMOVE_ITEM_CONFIRM).click();
+        // Removed 1 item
+        cy.get(shopping_list.ADDED_STRAWBERRIES).should('not.exist');
+        cy.get('ion-toast').should('exist').shadow().contains('Removed 1 item');
+    })
+
+    // did they handle shampoo = meat - spoiler alert: no
+    // it('should not put shampoo as meat', () => {
+    //     cy.wait(1000);
+    //     cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+    //     cy.wait(500);
+    //     cy.get(shopping_list.ITEM_TITLE_INPUT).type('shampoo');
+    //     cy.wait(500);
+    //     cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+    //     cy.get(shopping_list.SHAMPOO_MEAT).should('not.contain', 'Meats');
+    //     cy.wait(1000);
+    // })
+
+    // remove all items
+    it('should be able to clear an entire list', () => {
+        cy.get(shopping_list.ADD_ITEMS_CENTER).click();
+        cy.get(shopping_list.ITEM_TITLE_INPUT).type('strawberries');
+        cy.get(shopping_list.SECOND_ITEM_TITLE_INPUT).type('blueberries');
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        cy.wait(1000);
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.REMOVE_ALL_ITEMS).click();
+        cy.wait(1000);
+        cy.get(shopping_list.DELETE_ALL_CONFIRM).click();
+        cy.get(shopping_list.ADDED_STRAWBERRIES_MULTIPLE).should('not.exist');  
+        cy.get(shopping_list.ADDED_BLUEBERRIES_MULTIPLE).should('not.exist');
+    })
+
+    // group similar items (1 cup sugar, 1 tbsp sugar)
+    it('should be able to group similar items', () => {
+        cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+        cy.get(shopping_list.ITEM_TITLE_INPUT).type('1 cup sugar');
+        cy.get(shopping_list.SECOND_ITEM_TITLE_INPUT).type('1 tbsp sugar');
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        cy.wait(1000);
+        cy.get(shopping_list.COMBINED_SUGAR).contains('1 cup, 1 tablespoon sugar');
+        cy.get(shopping_list.COMBINED_SUGAR_DROPDOWN).click();
+        cy.wait(500);
+        cy.get(shopping_list.CUP_SUGAR).contains('1 cup sugar');
+        cy.get(shopping_list.TBSP_SUGAR).contains('1 tbsp sugar');
+    })
+
+    // group similar items - typo edition
+    it('should not group similar items when one has a typo', () => {
+        cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+        cy.get(shopping_list.ITEM_TITLE_INPUT).type('1 cup sugr');
+        cy.get(shopping_list.SECOND_ITEM_TITLE_INPUT).type('1 tbsp sugar');
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        cy.get(shopping_list.TYPO_GROCERY).should('exist');
+        cy.get(shopping_list.TYPO_GROCERY).contains('1 tbsp sugar');
+        cy.get(shopping_list.TYPO_UNCATEGORIZED).should('exist');
+        cy.get(shopping_list.TYPO_UNCATEGORIZED).contains('1 cup sugr');
+    })
+
+    // delete item immediately
+    it('should be able to delete items immediately', () => {
+        cy.get(shopping_list.ADD_ITEMS_BOTTOM_CORNER).click();
+        cy.get(shopping_list.ITEM_TITLE_INPUT).type('strawberries');
+        cy.get(shopping_list.ADD_ITEMS_ADD_TO_LIST).click();
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.DELETE_UPON_COMPLETION_TOGGLE).click();
+        cy.get(shopping_list.ADDED_STRAWBERRIES).click();
+        cy.get('ion-toast').should('exist').shadow().contains('Removed 1 item');
+        cy.get(shopping_list.ADDED_STRAWBERRIES).should('not.exist');
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.DELETE_UPON_COMPLETION_TOGGLE).click();
+        cy.wait(1000);
+    })
+})
+
+describe('shopping list - nondestructive options', () => {
+    beforeEach(() => {
+        cy.visit('/');
+        cy.get(landing_login.LOGIN_TOP).click();
+        cy.get(':nth-child(8) > .ng-untouched').type('echo30.dev@gmail.com');
+        cy.get(':nth-child(11) > .ng-untouched').type('password');
+        cy.get('.ion-padding > .md').click();
+        cy.get(sidebar.OPEN_SIDEBAR).click();
+        cy.get(sidebar.SHOPPING_LISTS).click();
+        cy.get(shopping_list.FIRST_IN_LIST, {timeout: 10000}).click();
+    })
+
+    // printing a shopping list
+    it('should be able to print a recipe', () => {
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.PRINT_RECIPE).should('not.be.disabled');
+        cy.wait(1000);
+    })
+
+    // mark item as completed
+    it('should be able to mark/unmark an item as completed', () => {
+        cy.get(shopping_list.HALF_CUP_MILK).click();
+        cy.get(shopping_list.COMPLETED_ITEMS_LIST).should('exist');
+        cy.get(shopping_list.HALF_CUP_MILK).should('not.contain', '1/2 cup milk');
+        cy.wait(1000);
+        cy.get(shopping_list.RESTORE_COMPLETED).click();
+        cy.get(shopping_list.COMPLETED_ITEMS_LIST).should('not.exist');
+        cy.get(shopping_list.ADDED_DAIRY).should('contain', '1/2 cup milk');
+    })
+
+    // sort by title
+    it('should be able to sort by title', () => {
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.SORT_BY).click();
+        cy.get(shopping_list.SORT_BY_POPUP).should('exist');
+        cy.get(shopping_list.SORT_BY_TITLE).click();
+        cy.get(shopping_list.SORT_BY_OK).click();
+    })
+
+    // sort by newest
+    it('should be able to sort by newest', () => {
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.SORT_BY).click();
+        cy.get(shopping_list.SORT_BY_POPUP).should('exist');
+        cy.get(shopping_list.CREATED_NEWEST).click();
+        cy.get(shopping_list.SORT_BY_OK).click();
+    })
+
+    // sort by oldest
+    it('should be able to sort by oldest', () => {
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.SORT_BY).click();
+        cy.get(shopping_list.SORT_BY_POPUP).should('exist');
+        cy.get(shopping_list.CREATED_OLDEST).click();
+        cy.get(shopping_list.SORT_BY_OK).click();
+    })
+
+    // group items by category toggle
+    it('should be able to toggle group items by category', () => {
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.GROUP_ITEMS_BY_CATEGORY_TOGGLE).click();
+        cy.get(shopping_list.DAIRY_HEADER).should('not.exist');
+        cy.get(shopping_list.GROCERY_HEADER).should('not.exist');
+        cy.get(shopping_list.PRODUCE_HEADER).should('not.exist');
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.GROUP_ITEMS_BY_CATEGORY_TOGGLE).click();
+        cy.get(shopping_list.DAIRY_HEADER).should('exist');
+        cy.get(shopping_list.GROCERY_HEADER).should('exist');
+        cy.get(shopping_list.PRODUCE_HEADER).should('exist');
+    })
+
+    // group similar items toggle
+    it('should be able to toggle group similar items', () => {
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.GROUP_SIMILAR_ITEMS_TOGGLE).click();
+        cy.get(shopping_list.GROUP_OPTION_CUP_SUGAR).contains('1 cup sugar');
+        cy.get(shopping_list.GROUP_OPTION_TBSP_SUGAR).contains('1 tbsp sugar');
+        cy.wait(1000);
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.GROUP_SIMILAR_ITEMS_TOGGLE).click();
+        cy.get(shopping_list.SUGAR_GROUPED).contains('1 cup, 1 tablespoon sugar');
+    })
+
+    // show added on toggle
+    it('should be able to show/hide when items were added', () => {
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.SHOW_ADDED_ON_TOGGLE).click();
+        cy.get(shopping_list.ADDED_ON_LABEL).contains('Added ');
+        cy.wait(1000);
+        cy.get(shopping_list.OPTIONS_NO_MULTISELECT).click();
+        cy.get(shopping_list.SHOW_ADDED_ON_TOGGLE).click();
+        cy.get(shopping_list.ADDED_ON_LABEL).should('not.contain', 'Added ');
+    })
+
 })
